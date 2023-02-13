4e890c2a2488b464b185cb3e8ecf3a776e46bde6
Vadim <Tomilov_Vadim@list.ru>
2023-02-14 02:56:35 +0500

    Added a modal product window

--- src/features/TopTable/Table/Tbody/Product/Product.jsx
+++ src/features/TopTable/Table/Tbody/Product/Product.jsx
@@ -1,18 +1,29 @@
 import ProductBtn from "../../../../../components/TopTable/Buttons/ProductBtn/ProductBtn";
+import AvailableInStock from "../../../../../components/TopTable/Indicators/AvailableInStock/AvailableInStock";
+import OutOfStock from "../../../../../components/TopTable/Indicators/OutOfStock/OutOfStock";
 import styles from "./Product.module.css";
 
 
 const Product = ({name, createdAt, price, count}) => {
+
+  const transformDate = createdAt
+            .replace("-", "/")
+            .replace("-", "/")
+            .slice(0, 10)
+            .split("/")
+            .reverse()
+            .join("/");
+  
   return (
     <tr className={styles.rowProduct}>
       <td>
         <ProductBtn name={name} />
       </td>
-      <td>{createdAt}</td>
+      <td>{transformDate}</td>
       <td>₽ {price}</td>
       <td>{count}</td>
       <td>
-        <p className={styles.indicatorYes}>Есть на складе</p>
+        {Number(count) > 0 ? <AvailableInStock /> : <OutOfStock />}
       </td>
     </tr>
   )
--- src/pages/TopTable/TopTableContainer.jsx
+++ src/pages/TopTable/TopTableContainer.jsx
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 import Header from "../../common/TopTable/Header/Header";
 import Table from "../../common/TopTable/Table/Table";
 import { requestProducts } from "../../stores/reducers/topTable-reducer";
+import styles from "./TopTable.module.css";
+import crossIcon from "../../assets/all-images/icons/cross.svg";
 
 
 const TopTableContainer = ({topTable, requestProducts}) => {
@@ -12,10 +14,31 @@ const TopTableContainer = ({topTable, requestProducts}) => {
     requestProducts();
   }, []);
 
+  const hideModal = () => {
+
+  };
+ 
   return (
     <>
       <Header />
       <Table products={topTable.products} />
+      <div className={styles.modalWrapper}>
+        <div className={styles.modalProductCard}>
+          <div className={styles.modalProductControl}>
+            <button onClick={hideModal}>
+              <img src={crossIcon} alt="crossIcon" />
+            </button>
+          </div>
+          <h3 className={styles.modalProductTitle}>Hat</h3>
+          <div className={styles.modalImgWrapper}>
+            <img src="https://loremflickr.com/640/480" alt="imageProduct" className={styles.modalImg} />
+          </div>
+          <p className={styles.modalProductPrice}>₽4.95</p>
+          <p className={styles.modalProductDescription}>В определенные исторические эпохи у ряда народов шляпа, как и другие элементы одежды, выполняла социальную функцию идентификации, указывая на принадлежность ее хозяин...</p>
+          <p className={styles.modalProductCount}>Количество: 1</p>
+          <a href="#" className={styles.modalProductLink}>Ссылка на товар</a>
+        </div>
+      </div>
     </>
   )
 };
